fe61622c29688de56a585df1d99fb92b51b2ba9a
Brian Paterni <bpaterni@gmail.com>
2019-05-18 16:28:17 -0500

    launch_app_local.sh: switch to default ports

--- launch_app_local.sh
+++ launch_app_local.sh
@@ -5,7 +5,7 @@ export ENABLE_MONKEY_PATCH_ALL=y
 export ORACLE_USER='sys'
 export ORACLE_PASS="Oradoc_db1"
 export ORACLE_HOST='localhost'
-export ORACLE_PORT=32770
+export ORACLE_PORT=1521
 export ORACLE_SERVICE_NAME='ORCLCDB.localdomain'
 
 export MSSQL_DRIVER="ODBC Driver 17 for SQL Server"
@@ -15,11 +15,11 @@ export MSSQL_HOST='127.0.0.1'
 export MSSQL_CATALOG='tempdb'
 
 export PSQL_USER='postgres'
-export PSQL_PASS='mysecretpass'
+export PSQL_PASS='mysecretpassword'
 export PSQL_HOST='localhost'
-export PSQL_PORT=32768
+export PSQL_PORT=5432
 export PSQL_SCHEMA='postgres'
 
 export FLASK_APP=app.py
 
-pipenv run flask run 
+pipenv run python app.py
